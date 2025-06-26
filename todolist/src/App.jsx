db5c8a83c85f840def28b8c318f33b49f121f2e7
hyunwoo1409 <kangs97@naver.com>
2025-06-26 18:37:10 +0900

    dd

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Wishlist from './pages/Wishlist';
